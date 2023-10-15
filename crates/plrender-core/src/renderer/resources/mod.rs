fdcc3fbec5e9adcdb0b5cce6501b7a2be079c3ca
Rafael Beckel <rgb@pupil-labs.com>
2023-10-15 07:17:40 +0200

    creates GPU resource: texture sampler

--- crates/plrender-core/src/renderer/resources/mod.rs
+++ crates/plrender-core/src/renderer/resources/mod.rs
@@ -1,3 +1,4 @@
 pub mod mesh;
+pub mod sampler;
 pub mod target;
 pub mod texture;
--- crates/plrender-core/src/renderer/resources/texture.rs
+++ crates/plrender-core/src/renderer/resources/texture.rs
@@ -1,4 +1,7 @@
-// This is essentially a TextureView
+use crate::renderer::resources::sampler::{SamplerBlueprint, SamplerSelector};
+use image::GenericImageView;
+use std::error::Error;
+
 pub struct Image {
     pub view: wgpu::TextureView,
     pub size: wgpu::Extent3d,
@@ -10,3 +13,146 @@ pub struct ImageInfo {
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct ImageRef(pub u32);
+
+#[derive(Debug)]
+pub struct Texture {
+    pub texture: wgpu::Texture,
+    pub view: wgpu::TextureView,
+    pub sampler: wgpu::Sampler,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct TextureRef(pub u32);
+
+impl Texture {
+    pub fn from_bytes(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        bytes: &[u8],
+        label: &str,
+    ) -> Result<Self, Box<dyn Error>> {
+        let img = image::load_from_memory(bytes)?;
+        Self::from_image(device, queue, &img, Some(label))
+    }
+
+    pub fn from_image(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        img: &image::DynamicImage,
+        label: Option<&str>,
+    ) -> Result<Self, Box<dyn Error>> {
+        let rgba = img.to_rgba8();
+        let dimensions = img.dimensions();
+
+        let size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: 1,
+        };
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            label,
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            // This is the same as with the SurfaceConfig. It
+            // specifies what texture formats can be used to
+            // create TextureViews for this texture. The base
+            // texture format (Rgba8UnormSrgb in this case) is
+            // always supported. Note that using a different
+            // texture format is not supported on the WebGL2
+            // backend.
+            view_formats: &[],
+        });
+
+        queue.write_texture(
+            // Tells wgpu where to copy the pixel data
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            // The actual pixel data
+            &rgba,
+            // The layout of the texture
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * dimensions.0),
+                rows_per_image: Some(dimensions.1),
+            },
+            size,
+        );
+
+        // We don't need to configure the texture view much, so let's let wgpu define it.
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        Ok(Self {
+            texture,
+            view,
+            sampler,
+        })
+    }
+
+    // We need the DEPTH_FORMAT for when we create the depth stage of
+    // the render_pipeline and for creating the depth texture itself.
+    pub const DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
+
+    pub fn create_depth_texture(
+        device: &wgpu::Device,
+        config: &wgpu::SurfaceConfiguration,
+        label: &str,
+    ) -> Self {
+        let size = wgpu::Extent3d {
+            // Our depth texture needs to be the same size as our screen if we
+            // want things to render correctly. We can use our config to make sure
+            // that our depth texture is the same size as our surface textures.
+            width: config.width,
+            height: config.height,
+            depth_or_array_layers: 1,
+        };
+        let desc = wgpu::TextureDescriptor {
+            label: Some(label),
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: Self::DEPTH_FORMAT,
+            // Since we are rendering to this texture,
+            // we need to add the RENDER_ATTACHMENT flag to it.
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        };
+        let texture = device.create_texture(&desc);
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let sampler = wgpu::Sampler::from_blueprint(
+            device,
+            SamplerBlueprint {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                filter: wgpu::FilterMode::Linear,
+                label: "Depth Texture Sampler".to_string(),
+                compare: Some(wgpu::CompareFunction::LessEqual),
+            },
+        );
+
+        Self {
+            texture,
+            view,
+            sampler,
+        }
+    }
+}
