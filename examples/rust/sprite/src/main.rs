6a0574f8e2370a69817f49d741b7117625bd8431
Rafael Beckel <rgb@pupil-labs.com>
2023-11-04 04:59:41 -0300

    reworks web and Rust examples
    note: examples are still WIP

--- examples/rust/sprite/src/main.rs
+++ examples/rust/sprite/src/main.rs
@@ -1,15 +1,8 @@
 use instant::{Duration, Instant};
-use plrender::{window::WindowOptions, RenderOptions, RenderPass};
-
-//TODO: a mechanism like this should be a part of the engine
-struct Animator {
-    map: plrender::loader::SpriteMap,
-    cell_counts: mint::Vector2<usize>,
-    duration: Duration,
-    sprite: plrender::EntityId,
-    current: mint::Point2<usize>,
-    moment: Instant,
-}
+use plrender::{
+    animation::Animator,
+    renderer::{RenderOptions, Renderer},
+};
 
 #[repr(usize)]
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -27,85 +20,35 @@ impl Default for State {
     }
 }
 
-impl Animator {
-    fn update_uv(&mut self, scene: &mut plrender::Scene) {
-        let uv_range = self.map.at(self.current);
-        scene
-            .world
-            .get::<&mut plrender::Sprite>(self.sprite)
-            .unwrap()
-            .uv = Some(uv_range);
-    }
-
-    fn switch(&mut self, state: State, scene: &mut plrender::Scene) {
-        self.moment = Instant::now();
-        self.current.x = 0;
-        self.current.y = state as usize;
-        self.update_uv(scene);
-    }
-
-    fn tick(&mut self, scene: &mut plrender::Scene) {
-        if self.moment.elapsed() < self.duration {
-            return;
-        }
-
-        self.current.x += 1;
-        self.moment = Instant::now();
-        if self.current.x == self.cell_counts.x {
-            self.current.x = 0;
-            self.current.y = State::Idle as usize;
-            // don't update the scene here, so that
-            // input can have a chance to transition
-            // to something other than `Idle`.
-        } else {
-            self.update_uv(scene);
-        }
-    }
+fn main() {
+    pollster::block_on(run());
 }
 
-fn main() {
-    use plrender::window::{Event, Key, Window};
+async fn run() {
+    use plrender::app::window::{IsWindow, Window};
 
-    // This will be created by the user in their envieronment
-    // or provided by our library if the "Window" feature is enabled
-    //
-    // **Example JS:**
-    // ```javascript
-    //     let canvas = document.getElementById("canvas");
-    //     // or
-    //     let canvas = plrender::Canvas();
-    //     // or
-    //     let canvas = plrender::Canvas("#canvas");
-    //     let canvas = plrender::Canvas({width: 800, height: 600}});
-    // ````
-    //
-    // **Example Python:**
-    // ```python
-    //     import pygame
-    //     pygame.init()
-    //     pygame.display.set_mode((800, 600))
-    //     window = pygame.display.get_surface()
-    //     # or
-    //     window = plrender::Window()
-    //     # or
-    //     window = plrender::Window(size: (800, 600), title: "My Window")
-    // ```
-    let window = Window::new(WindowOptions {
-        title: Some("Sprite".to_string()),
-        size: Some((400, 300)),
-        ..Default::default()
-    })
-    .unwrap();
+    // You can use the App to create multiple Windows
+    // let app = plrender::App::default();
+    // let window1 = plrender::Window::new(&app, WindowOptions::default());
+    // let window2 = plrender::Window::new(&app, WindowOptions::default());
 
-    let mut renderer = pollster::block_on(plrender::Renderer::new(RenderOptions {
-        targets: vec![window],
-    }))
-    .unwrap();
-
+    let window = Window::default().set_title("Sprite").set_size((800, 600));
     let mut scene = plrender::Scene::new();
 
+    scene.add_target(window);
+
+    // Renderer is implicitly created by Window::default() or App::add_window()
+    // let mut renderer = Renderer::new(RenderOptions {
+    //     targets: Some(vec![window]),
+    //     ..Default::default()
+    // })
+    // .await
+    // .unwrap();
+
     // this has to be part of the scene and created
-    // by default without user input
+    // by default without user input.
+    //
+    // but how to pass the right arguments to it?
     let camera = plrender::Camera {
         projection: plrender::Projection::Orthographic {
             // the sprite configuration is not centered
@@ -117,8 +60,9 @@ fn main() {
 
     // this needs to be abstracted away. Ideally, we should have some
     // preset render passes as attributes of the renderer
-    let mut pass = plrender::renderpass::Flat::new(&mut renderer);
+    let mut pass = plrender::renderer::renderpass::Flat2D::new(&mut renderer);
 
+    // It's currently like this:
     let image = renderer
         .load_image(format!(
             "{}/assets/images/pickachu.png",
@@ -128,8 +72,20 @@ fn main() {
 
     let sprite = scene.add_sprite(image).build();
 
+    // @TODO But should be like this:
+    // let sprite = plrender::Sprite::new(
+    //     &mut renderer,
+    //     format!(
+    //         "{}{}",
+    //         env!("CARGO_MANIFEST_DIR"),
+    //         "/assets/images/pickachu.png"
+    //     ),
+    // );
+    //
+    // scene.add(sprite);
+
     let mut anim = Animator {
-        map: plrender::loader::SpriteMap {
+        map: plrender::asset::SpriteMap {
             origin: mint::Point2 { x: 0, y: 0 },
             cell_size: mint::Vector2 { x: 96, y: 96 },
         },
@@ -139,36 +95,40 @@ fn main() {
         moment: Instant::now(),
         sprite,
     };
-    anim.switch(State::Idle, &mut scene);
+    anim.switch(State::Idle as usize, &mut scene);
 
-    window.on_resize(|width, height| {
-        // renderer.resize(width, height);
+    // @TODO
+    window.on("resize", |Event::Resize { width, height }| {
+        let target = renderer.targets().get_mut(window.id()).unwrap();
+        target.resize(
+            &renderer,
+            wgpu::Extent3d {
+                width,
+                height,
+                depth_or_array_layers: 1,
+            },
+        );
     });
 
-    window.run(move |event| match event {
-        Event::Resize { width, height } => {
-            // renderer.resize(width, height);
-        }
-        Event::Keyboard { key, pressed: true } => {
-            let new_state = match key {
-                Key::Up => Some(State::Jump),
-                Key::Down => Some(State::Lie),
-                Key::Space => Some(State::Kick),
-                Key::Left => Some(State::MoveLeft),
-                Key::Right => Some(State::MoveRight),
-                _ => None,
-            };
-            if let Some(state) = new_state {
-                if anim.current.y != state as usize || state == State::Kick {
-                    anim.switch(state, &mut scene);
-                }
+    window.on("keydown", |Event::Keyboard { key, pressed: true }| {
+        let new_state = match key {
+            Key::Up => Some(State::Jump),
+            Key::Down => Some(State::Lie),
+            Key::Space => Some(State::Kick),
+            Key::Left => Some(State::MoveLeft),
+            Key::Right => Some(State::MoveRight),
+            _ => None,
+        };
+        if let Some(state) = new_state {
+            if anim.current.y != state as usize || state == State::Kick {
+                anim.switch(state, &mut scene);
             }
         }
-        Event::Draw => {
-            anim.tick(&mut scene);
-            //                                 @TODO
-            renderer.render(&mut pass, &scene, &scene.camera());
-        }
-        _ => {}
-    })
+    });
+
+    window.on("draw", || {
+        anim.tick(&mut scene);
+        //                                 @TODO
+        renderer.render(&mut pass, &scene, &scene.camera());
+    });
 }
