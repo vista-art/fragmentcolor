b8d283ea0e9560195581cf7cc505417c67249a1a
Rafael Beckel <rafaelbeckel@gmail.com>
2025-03-13 13:22:53 +0100

    updates pyython example

--- fragmentcolor_python/main.py
+++ fragmentcolor_python/main.py
@@ -1,3 +1,31 @@
-import fragmentcolor
+from fragmentcolor import FragmentColor as fc, Shader
+from rendercanvas.auto import RenderCanvas, loop
 
-fragmentcolor.guess_the_number()
+canvas = RenderCanvas(size=(800, 600))
+renderer, target = fc.init(canvas)
+
+with open("circle.wgsl") as file:
+    source = file.read()
+
+circle = Shader(source)
+circle.set("resolution", [800, 600])
+circle.set("circle.radius", 200.0)
+circle.set("circle.color", [1.0, 0.0, 0.0, 0.8])
+circle.set("circle.border", 20.0)
+
+renderer.render(circle, target)
+
+
+@canvas.resize
+def resize(w, h):
+    circle.set("resolution", (w, h))
+
+
+@canvas.request_draw
+def animate():
+    circle.set("position", [0.0, 0.0])
+    renderer.render(circle, target)
+
+
+# Enter main rendering loop
+loop.run()
