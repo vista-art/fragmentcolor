14518738e8581f556199967f21f07f28698250c4
Rafael Beckel <rafaelbeckel@gmail.com>
2025-03-20 17:03:43 +0100

    convenience script to bump the library version

--- bump_version.py
+++ bump_version.py
@@ -5,7 +5,13 @@ import sys
 
 
 def bump_version(file_path, bump_type):
-    with open(file_path, 'r') as file:
+    """Bumbs fragmentcolor version
+
+    Args:
+        file_path (_type_): file to bump version in
+        bump_type (_type_): type of bump to perform (major, minor, patch)
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
     version_pattern = (
@@ -19,25 +25,36 @@ def bump_version(file_path, bump_type):
 
     major, minor, patch = map(int, match.groups())
 
-    if bump_type == 'm':
-        minor += 1
-        patch = 0
-    elif bump_type == 'M':
+    # bumps major version
+    if bump_type == 'M':
         major += 1
         minor = 0
         patch = 0
-    elif bump_type == 'd':
-        if minor > 0:
-            minor -= 1
+    # bumps minor version
+    elif bump_type == 'm':
+        minor += 1
         patch = 0
+    # bumps patch version
+    elif bump_type == 'P':
+        patch += 1
+
+    # decreases major version
     elif bump_type == 'D':
         if major > 0:
             major -= 1
         minor = 0
         patch = 0
+    # decreases minor version
+    elif bump_type == 'd':
+        if minor > 0:
+            minor -= 1
+        patch = 0
+    # decreases patch version
     elif bump_type == 'p':
         if patch > 0:
             patch -= 1
+
+    # bumps the patch version by default (most common use case)
     else:
         patch += 1
 
@@ -48,13 +65,13 @@ def bump_version(file_path, bump_type):
         content
     )
 
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         file.write(new_content)
 
     print(f"Bumped version in {file_path} to {new_version}")
 
 
 if __name__ == "__main__":
-    bump_type = sys.argv[1] if len(sys.argv) > 1 else ''
-    bump_version('Cargo.toml', bump_type)
-    bump_version('pyproject.toml', bump_type)
+    bump = sys.argv[1] if len(sys.argv) > 1 else ''
+    bump_version('Cargo.toml', bump)
+    bump_version('pyproject.toml', bump)
