d0981b1c312c4411239d3b6fb0cc8b542cf24d57
Rafael Beckel <rafaelbeckel@gmail.com>
2025-09-09 19:30:26 +0200

    tiny python doc fix

--- platforms/python/examples/core/pass/set_clear_color.py
+++ platforms/python/examples/core/pass/set_clear_color.py
@@ -6,8 +6,8 @@ target = renderer.create_texture_target([64, 64])
 
 shader = Shader.default()
 rpass = Pass("solid background")
-pass.add_shader(shader)
+rpass.add_shader(shader)
 
-pass.set_clear_color([0.1, 0.2, 0.3, 1.0])
+rpass.set_clear_color([0.1, 0.2, 0.3, 1.0])
 
-renderer.render(pass, target)
+renderer.render(rpass, target)
